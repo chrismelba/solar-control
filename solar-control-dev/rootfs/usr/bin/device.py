49135dbe478f99d82bb8b6faaefc8556a9b94e0e
Chris Carter <chrismelba@gmail.com>
2025-05-29 10:28:34 +1000

    debug energy api

--- solar-control-dev/rootfs/usr/bin/device.py
+++ solar-control-dev/rootfs/usr/bin/device.py
@@ -133,7 +133,7 @@ class Device:
             logger.debug(f"Current energy reading: {current_energy}")
             
             # Calculate energy delivered today
-            self.energy_delivered_today = current_energy - dawn_energy
+            self.energy_delivered_today = current_energy 
             logger.info(f"Updated energy delivered for {self.name}: {self.energy_delivered_today} Wh (Current: {current_energy}, Dawn: {dawn_energy})")
             
         except requests.exceptions.RequestException as e:
