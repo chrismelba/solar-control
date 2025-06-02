7a0fa68abdb3b01b3ac6ad506ad7014e86dbfece
Chris Carter <chrismelba@gmail.com>
2025-06-02 20:59:06 +1000

    added legacy to dev

--- solar-control-dev/rootfs/usr/bin/device.py
+++ solar-control-dev/rootfs/usr/bin/device.py
@@ -91,6 +91,13 @@ class Device:
             if field in converted_data:
                 converted_data[field] = cls._safe_convert(converted_data[field], target_type)
         
+        # Remove legacy fields that are no longer supported
+        legacy_fields = ['last_power_update', 'last_dawn_reset']
+        for field in legacy_fields:
+            if field in converted_data:
+                logger.info(f"Removing legacy field '{field}' from device data")
+                del converted_data[field]
+        
         return cls(**converted_data)
 
     def update_energy_delivered(self) -> None:
